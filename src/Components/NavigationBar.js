8cf1bb4ea19fcd15ab26d21a99bc1ac9ecf8d377
Ryno <r_romario@msn.com>
2022-02-28 15:45:41 +0100

    Start

---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+
+export default function NavigationBar() {
+  return (
+    <div>
+        <Button variant='primary'>test</Button>
+    </div>
+  )
+}
